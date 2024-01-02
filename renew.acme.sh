ffaad3017eedc75022afce5d61e1abd05d46af21
Erik Schweighofer <eschweighofer@gmail.com>
2024-01-02 23:36:50 +0100

    Update renew.acme.sh

--- renew.acme.sh
+++ renew.acme.sh
@@ -3,11 +3,12 @@
 usage() {
     echo "Usage: $0 -d <mydomain.com> [-d <additionaldomain.com>] [options ...]
 Options:
-    -h, --help          Show this help message.
-    -d, --domain        Specifies domain for cert, allowed multiple times.
-    -f, --force         Force cert renewal.
-    --debug [0|1|2|3]   Output debug info. Defaults to 1 if argument is omitted.
-    --staging, --test   Use staging server, for testing.
+    -h, --help              Show this help message.
+    -d, --domain            Specifies domain for cert, allowed multiple times.
+    -f, --force             Force cert renewal.
+    --dns dns_hook          Use dns api. Defaults to dns_azure when argument is omitted
+    --debug [0|1|2|3]       Output debug info. Defaults to 1 if argument is omitted.
+    --staging, --test       Use staging server, for testing.
 " 1>&2; exit 1;
 }
 
@@ -49,6 +50,14 @@ while [ ${#} -gt 0 ]; do
             DOMAIN+=("$2")
             shift
             ;;
+        --dns)
+            if [ -z "$2" ] || [ "${2:0:1}" = "-" ]; then
+                DNS_HOOK="--dns dns_azure"
+            else
+                DNS_HOOK="--dns ${2}"
+                shift
+            fi
+            ;;        
         --debug)
             if [ -z "$2" ] || [ "${2:0:1}" = "-" ]; then
                 DEBUG="--debug 1"
@@ -81,9 +90,16 @@ done
 
 ACMEHOME=/config/.acme.sh
 
-mkdir -p $ACMEHOME/webroot
+log "Stopping GUI service."
+if [ -e "/var/run/lighttpd.pid" ]; then
+    kill_and_wait $(cat /var/run/lighttpd.pid)
+fi
+    
+if [ -z "$DNS_HOOK" ]; then
+    WEB="-w $ACMEHOME/webroot"
+    mkdir -p $ACMEHOME/webroot
 
-(
+    (
 cat <<EOF
 server.modules = ( "mod_accesslog" )
 server.document-root = "$ACMEHOME/webroot"
@@ -94,37 +110,37 @@ server.pid-file = "$ACMEHOME/lighttpd.pid"
 server.errorlog = "/dev/null"
 accesslog.filename = "$ACMEHOME/lighttpd.log"
 EOF
-) >$ACMEHOME/lighttpd.conf
+    ) >$ACMEHOME/lighttpd.conf
 
-log "Stopping GUI service."
-if [ -e "/var/run/lighttpd.pid" ]; then
-    kill_and_wait $(cat /var/run/lighttpd.pid)
+    log "Starting temporary ACME challenge service."
+    /usr/sbin/lighttpd -f $ACMEHOME/lighttpd.conf
+
+    /sbin/iptables -I INPUT 1 -p tcp -m comment --comment TEMP_LETSENCRYPT -m tcp --dport 80 -j ACCEPT
+    /sbin/ip6tables -I INPUT 1 -p tcp -m comment --comment TEMP_LETSENCRYPT -m tcp --dport 80 -j ACCEPT
+    /sbin/iptables -t nat -I PREROUTING 1 -p tcp -m comment --comment TEMP_LETSENCRYPT -m tcp --dport 80 -j ACCEPT
 fi
-
-log "Starting temporary ACME challenge service."
-/usr/sbin/lighttpd -f $ACMEHOME/lighttpd.conf
-
-/sbin/iptables -I INPUT 1 -p tcp -m comment --comment TEMP_LETSENCRYPT -m tcp --dport 80 -j ACCEPT
-/sbin/ip6tables -I INPUT 1 -p tcp -m comment --comment TEMP_LETSENCRYPT -m tcp --dport 80 -j ACCEPT
-/sbin/iptables -t nat -I PREROUTING 1 -p tcp -m comment --comment TEMP_LETSENCRYPT -m tcp --dport 80 -j ACCEPT
 
 # trick sudo detection in acme.sh
 unset SUDO_COMMAND
-$ACMEHOME/acme.sh --issue $DOMAINARG -w $ACMEHOME/webroot --home $ACMEHOME \
+$ACMEHOME/acme.sh --issue $DOMAINARG $WEB --home $ACMEHOME $DNS_HOOK \
 --reloadcmd "cat \$CERT_PATH \$CERT_KEY_PATH > /config/ssl/server.pem; cp \$CA_CERT_PATH /config/ssl/ca.pem" \
 ${STAGING} ${FORCE} ${DEBUG}
-/sbin/iptables -D INPUT -p tcp -m comment --comment TEMP_LETSENCRYPT -m tcp --dport 80 -j ACCEPT
-/sbin/ip6tables -D INPUT -p tcp -m comment --comment TEMP_LETSENCRYPT -m tcp --dport 80 -j ACCEPT
-/sbin/iptables -t nat -D PREROUTING 1
 
-log "Stopping temporary ACME challenge service."
-if [ -e "$ACMEHOME/lighttpd.pid" ]; then
-    kill_and_wait $(cat $ACMEHOME/lighttpd.pid)
+if [ -z "$DNS_HOOK" ]; then
+    /sbin/iptables -D INPUT -p tcp -m comment --comment TEMP_LETSENCRYPT -m tcp --dport 80 -j ACCEPT
+    /sbin/ip6tables -D INPUT -p tcp -m comment --comment TEMP_LETSENCRYPT -m tcp --dport 80 -j ACCEPT
+    /sbin/iptables -t nat -D PREROUTING 1
+
+    log "Stopping temporary ACME challenge service."
+    if [ -e "$ACMEHOME/lighttpd.pid" ]; then
+        kill_and_wait $(cat $ACMEHOME/lighttpd.pid)
+    fi
 fi
-
+    
 log "Starting GUI service."
 if [ -x "/bin/systemctl" ]; then
     /bin/systemctl start lighttpd.service
 else
     /usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf
+fi
 fi
