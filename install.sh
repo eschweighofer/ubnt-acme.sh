bbbb4854ef2cbb77f425afb3cd4e81afaeb938fd
Erik Schweighofer <eschweighofer@gmail.com>
2024-01-02 23:11:13 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,11 @@
 ACMEHOME=/config/.acme.sh
 CA_BUNDLE=/config/ssl/cacert.pem
 
-mkdir -p ${ACMEHOME} /config/scripts /config/ssl
+mkdir -p ${ACMEHOME}/dnsapi /config/scripts /config/ssl
 curl -o ${ACMEHOME}/acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
-curl -o /config/scripts/renew.acme.sh https://raw.githubusercontent.com/j-c-m/ubnt-letsencrypt/master/renew.acme.sh
-chmod 755 ${ACMEHOME}/acme.sh /config/scripts/renew.acme.sh
+curl -o ${ACMEHOME}/dnsapi/dns_azure.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/dnsapi/dns_azure.sh
+curl -o /config/scripts/renew.acme.sh https://raw.githubusercontent.com/eschweighofer/ubnt-acme.sh/master/renew.acme.sh
+chmod 755 ${ACMEHOME}/acme.sh ${ACMEHOME}/dnsapi/dns_azure.sh /config/scripts/renew.acme.sh
 
 if [ -s ${CA_BUNDLE} ]; then
     curl https://curl.se/ca/cacert.pem -z ${CA_BUNDLE} -o ${CA_BUNDLE}
